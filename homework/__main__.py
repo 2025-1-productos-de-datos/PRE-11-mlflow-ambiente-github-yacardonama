b9df4af041c7545b5ced69485898494bda2bfb5f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-09 09:42:04 -0500

    Initial commit

--- homework/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Entry point."""
-
-from .src.main import main
-
-if __name__ == "__main__":
-    main()
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,4 +1,31 @@
 """Grading"""
 
+import os
+import subprocess
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
 def test_01():
-    pass
+
+    # Test if the homework script runs without errors
+    try:
+        subprocess.run(
+            [
+                "source",
+                "run.sh",
+            ],
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Error running the homework script: {e}")
+
+    # Ensure the mlruns directory exists
+    assert os.path.exists("mlruns"), "mlruns directory does not exist."
+
+    # Check if there are any experiments saved in mlruns/
+    experiments = [
+        d for d in os.listdir("mlruns") if os.path.isdir(os.path.join("mlruns", d))
+    ]
+    assert len(experiments) > 0, "No experiments found in mlruns directory."
